6d882fff8c3d49b2665b7b6a15440c5cd980b972
AMAL BENNY <108413635+tecktrio@users.noreply.github.com>
2023-06-15 14:15:36 +0530

    Update and rename cdn3.js to cdn4.js

--- cdn3.js
+++
@@ -1,7 +0,0 @@
-     function playAudio(text){
-                let utternance = new SpeechSynthesisUtterance(text)
-                console.log('speaking',text)
-                speechSynthesis.speak(utternance)
-                console.log('done');
-                
-             }
---
+++ cdn4.js
@@ -0,0 +1,10 @@
+     window.onload = function(){
+              <!-- cotrol bot -->
+               let div = document.getElementById("mgif")
+               div.innerHtml = "<div style="position: fixed;width: 100vw;bottom: 0;z-index: 50;"> 
+                                   <marquee direction="right" scrollamount="10">
+                                   <img src="https://webstockreview.net/images/clipart-dinosaur-dinosaur-extinction-18.gif" width="200px">
+                                   </marquee>
+                                   </div>"
+              
+             }
