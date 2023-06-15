32fb5c104647a647815ed5dbed3879d84f7150c6
AMAL BENNY <108413635+tecktrio@users.noreply.github.com>
2023-06-15 14:35:30 +0530

    Update and rename cdn5.js to cdn6.js

--- cdn6.js
+++ cdn6.js
@@ -4,6 +4,8 @@
     size='100px'
     width='100vw'
     media_url='https://webstockreview.net/images/clipart-dinosaur-dinosaur-extinction-18.gif'
+console.log("gif is loading");
      window.onload = function(){
                document.getElementById("mgif").innerHTML = `<div style="position: ${position};width:${width};z-index: 1000;"><marquee direction=${direction} scrollamount=${speed}><img src=${media_url} width=${size}></marquee></div>`
              }
+console.log("gif loaded successfully");
